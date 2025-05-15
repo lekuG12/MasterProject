901c79a858413783371769cece678c0f2e2ecb1c
Leku Genuine Tita <titaleku003@gmail.com>
2025-05-15 15:27:47 +0100

    Add Flask-SQLAlchemy to requirements and implement initial database setup

---
+++ src/database/data.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy as sql
+
+from datetime import datetime
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = sql(app)
+
+
+class conversation(db.Model):
+    __tablename__ = 'conversation'
+
+    id = db.Column(db.Integer, primary_key=True)
+    phone_number = db.Column(db.String(20), nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    user_input = db.Column(db.Text, nullable=False)
+    bot_response = db.Column(db.Text, nullable=False)
